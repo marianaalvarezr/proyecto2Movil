f697315cabf675eee2214ab7cde5254a8bbbfc0e
Elizabeth <lenguaje.pecz@gmail.com>
2023-07-19 21:11:14 -0500

    se arreglan idionas y crea login

--- app/src/main/java/com/example/turisteandov2/Home.java
+++ app/src/main/java/com/example/turisteandov2/Home.java
@@ -3,12 +3,15 @@ package com.example.turisteandov2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import java.util.Locale;
 
 public class Home extends AppCompatActivity {
     //ZONA ATRIBUTOS
@@ -46,6 +49,19 @@ public class Home extends AppCompatActivity {
         });
     }
 
+    public void cambiarIdioma(String idioma){
+        //configurar el lenguaje del telefono
+        Locale lenguaje=new Locale(idioma);
+        Locale.setDefault(lenguaje);
+
+        //configuramos globalmente el telefono
+        Configuration configuracionTelefono=getResources().getConfiguration();
+        configuracionTelefono.locale=lenguaje;
+
+        //Ejecutamos la configuracion
+        getBaseContext().getResources().updateConfiguration(configuracionTelefono,getBaseContext().getResources().getDisplayMetrics());
+    }
+
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
@@ -55,13 +71,19 @@ public class Home extends AppCompatActivity {
         int itemSeleccionado = item.getItemId();
         switch (itemSeleccionado) {
             case (R.id.opcion1):
-                Toast.makeText(this, "quiero que este en ingles", Toast.LENGTH_SHORT).show();
+                this.cambiarIdioma("en");
+                Intent intentIngles=new Intent(Home.this, Home.class);
+                startActivity(intentIngles);
                 break;
             case (R.id.opcion2):
-                Toast.makeText(this, "quiero que este en español", Toast.LENGTH_SHORT).show();
+                this.cambiarIdioma("es");
+                Intent intentEspanol=new Intent(Home.this, Home.class);
+                startActivity(intentEspanol);
                 break;
             case (R.id.opcion3):
-                Toast.makeText(this, "quiero que este en italiano", Toast.LENGTH_SHORT).show();
+                this.cambiarIdioma("it");
+                Intent intentItaliano=new Intent(Home.this, Home.class);
+                startActivity(intentItaliano);
                 break;
             case (R.id.opcion4):
                Intent intent = new Intent(Home.this,Acerca.class);
@@ -70,7 +92,5 @@ public class Home extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-    public void cambiarIdioma (String idioma){
 
-    }
 }
--- app/src/main/java/com/example/turisteandov2/Login.java
+++ app/src/main/java/com/example/turisteandov2/Login.java
@@ -2,6 +2,7 @@ package com.example.turisteandov2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
 
@@ -20,5 +21,15 @@ public class Login extends AppCompatActivity {
         //capturando datos de los etittext
         String nombreUsuario=cajaUsuario.getText().toString();
         String contrasena=cajaContrasena.getText().toString();
+
+        Intent intent=new Intent(Login.this,Home.class);
+
+        //pasando datos ala nueva actividad
+
+        intent.putExtra("nombre",nombreUsuario);
+        intent.putExtra("password",contrasena);
+
+        startActivity(intent);
+
     }
 }
